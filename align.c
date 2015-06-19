d9b9f50b5babf84995f7ad163bdeb1e08e46f981
me <nmapsy@gmail.com>
2015-06-19 15:37:38 -0400

    align.c: Make get_diffs() case-insensitive, change order of arguments.

--- align.c
+++ align.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 // Take an existing alignment and consensus and compute the number of differences between each
 // sequence and the consensus.
-int *get_diffs(int n_seqs, char *seqs[], char *cons) {
+int *get_diffs(char *cons, char *seqs[], int n_seqs) {
   int diffs[n_seqs];
-  int i, j;
+  int i = 0;
+  // Uppercase the consensus.
+  while (cons[i] != 0) {
+    cons[i] = toupper(cons[i]);
+    i++;
+  }
+  int j;
   char *seq;
   // Loop through the sequences in the alignment.
   for (i = 0; i < n_seqs; i++) {
@@ -14,7 +21,7 @@ int *get_diffs(int n_seqs, char *seqs[], char *cons) {
     diffs[i] = 0;
     // Compare each base of the sequence to the consensus.
     while (seq[j] != 0 && cons[j] != 0) {
-      if (seq[j] != cons[j]) {
+      if (toupper(seq[j]) != cons[j]) {
         diffs[i]++;
       }
       j++;
@@ -22,4 +29,3 @@ int *get_diffs(int n_seqs, char *seqs[], char *cons) {
   }
   return diffs;
 }
-
