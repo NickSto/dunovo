fc7467b6a5232693a5884c03f0308bb982462462
me <nmapsy@gmail.com>
2015-07-04 06:52:35 -0400

    pipeline.sh: Version 2 of awk script for sorting barcodes independent of alpha-beta order.

--- pipeline.sh
+++ pipeline.sh
@@ -10,28 +10,40 @@ set -ue
 # the first 12bp of read 2. Columns 3 and 6 are the sequences of reads 1 and 2; the 12bp barcode and
 # 5bp constant sequence are removed.
 AwkScript1='{print substr($2, 1, 12) substr($6, 1, 12), $1, substr($2, 18), $4, $5, substr($6, 18), $8}'
-# This translates the barcode sequence into a code that is independent of the order of the alpha and
-# beta portions. So the output should be the same for a beta-alpha or an alpha-beta from the same
-# fragment.
-# Warning: This assumes all bases in the barcode are one of (uppercase) "ACGT". Characters not in
-# that set will be ignored!
+# This converts the barcode sequence into a canonical form that should be the same regardless of the
+# order of the alpha and beta portions. It does this simply by sorting them based on their character
+# values. So the output should be the same for a beta-alpha or an alpha-beta from the same fragment.
 AwkScript2='BEGIN {
+  FS = "\t"; OFS = "\t";
   BAR_LEN = 24;
   HALF = int(BAR_LEN/2);
-  TR["AA"] = "A"; TR["AC"] = "B"; TR["AG"] = "D"; TR["AT"] = "E";
-  TR["CA"] = "B"; TR["CC"] = "C"; TR["CG"] = "F"; TR["CT"] = "H";
-  TR["GA"] = "D"; TR["GC"] = "F"; TR["GG"] = "G"; TR["GT"] = "I";
-  TR["TA"] = "E"; TR["TC"] = "H"; TR["TG"] = "I"; TR["TT"] = "T";
+  # Fill an array mapping each character to its ASCII value.
+  for (i = 0; i < 128; i++) {
+    char = sprintf("%c", i);
+    ORD[char] = i;
+  }
+}
+function get_sum(seq) {
+  sum = 0;
+  for (i = 1; i <= length(seq); i++) {
+    sum += ORD[substr(seq, i, 1)];
+  }
+  return sum;
 }
 # Expect first and third column to be the FASTA header, like ">AAAAAAAACCAAAACCATCTCTTA.2".
 length($1) == BAR_LEN + 3 && substr($1, 2, BAR_LEN) == substr($3, 2, BAR_LEN) {
-  code = ""
-  for (i = 2; i <= HALF+1; i++) {
-    j = i + HALF;
-    pair = substr($1, i, 1) substr($1, j, 1);
-    code = code TR[pair];
+  alpha = substr($1, 2, HALF);
+  beta = substr($1, HALF+2, HALF);
+  alpha_sum = get_sum(alpha);
+  beta_sum = get_sum(beta);
+  if (alpha_sum < beta_sum) {
+    canonical = alpha beta;
+    order = "ab";
+  } else {
+    canonical = beta alpha;
+    order = "ba";
   }
-  print code, substr($1, 2, BAR_LEN), $2, $4;
+  print canonical, order, $2, $4;
 }'
 
 function main {
