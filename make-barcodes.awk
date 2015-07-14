5fc343f7657e534596dcf2aaaa7bd9e6cb01e873
me <nmapsy@gmail.com>
2015-07-14 14:30:03 -0400

    pipeline.sh, make-barcodes.awk: Awk commenting and readability, move code out of pipeline.sh.

--- make-barcodes.awk
+++ make-barcodes.awk
@@ -1,3 +1,21 @@
+# The awk code that transforms the one-line fastq record pair into the output that can be sorted
+# by barcode.
+# The input must have 8 columns: the 4 FASTQ lines for both reads in a read pair.
+# Output columns:
+#   1: the barcode, put into a canonical form
+#   2: the order of the barcode halves ("ab" or "ba")
+#   3: read1 name
+#   4: sequence of read 1, minus the 12bp barcode and 5bp constant sequence
+#   5: read1 quality scores, minus the same first 17bp
+#   6: read2 name
+#   7: sequence of read 2, minus the first 17bp
+#   8: read2 quality scores, minus the first 17bp
+# The canonical form of the barcode is composed of the first 12bp of each read. The 12bp from the
+# first read is the "alpha" and the one from the second is the "beta". The barcode is formed by
+# concatenating them in an order determined by summing the ASCII values of each sequence, then
+# putting the one with a lower sum first. If the alpha is first, the value of the second column (the
+# order) is "ab", and "ba" if the beta is first.
+
 BEGIN {
   FS = "\t"; OFS = "\t";
   if (! BAR_LEN) {
@@ -13,6 +31,7 @@ BEGIN {
     ORD[char] = i;
   }
 }
+
 function get_sum(seq) {
   sum = 0;
   for (i = 1; i <= length(seq); i++) {
@@ -20,6 +39,7 @@ function get_sum(seq) {
   }
   return sum;
 }
+
 {
   alpha = substr($2, 1, HALF);
   beta = substr($6, 1, HALF);
@@ -32,9 +52,11 @@ function get_sum(seq) {
     barcode = beta alpha;
     order = "ba";
   }
-  read1 = substr($2, HALF + INVARIANT + 1);
-  read2 = substr($6, HALF + INVARIANT + 1);
-  # $1 = read1 name | $4 = read1 quality | $5 = read2 name | $8 = read2 quality
-  print barcode, order, $1, read1, $4, $5, read2, $8;
+  name1 = $1;
+  name2 = $5;
+  seq1 = substr($2, HALF + INVARIANT + 1);
+  seq2 = substr($6, HALF + INVARIANT + 1);
+  qual1 = substr($4, HALF + INVARIANT + 1);
+  qual2 = substr($8, HALF + INVARIANT + 1);
+  print barcode, order, name1, seq1, qual1, name2, seq2, qual2;
 }
-
--- pipeline.sh
+++ pipeline.sh
@@ -5,47 +5,6 @@ if [ x$BASH = x ] || [ ! $BASH_VERSINFO ] || [ $BASH_VERSINFO -lt 4 ]; then
 fi
 set -ue
 
-# The awk code that transforms the one-line fastq record pair into the output that can be sorted
-# by barcode. Column 1 is the barcode, which is simply the first 12bp of read 1 concatenated with
-# the first 12bp of read 2. Columns 3 and 6 are the sequences of reads 1 and 2; the 12bp barcode and
-# 5bp constant sequence are removed.
-AwkScript1='{print substr($2, 1, 12) substr($6, 1, 12), $1, substr($2, 18), $4, $5, substr($6, 18), $8}'
-# This converts the barcode sequence into a canonical form that should be the same regardless of the
-# order of the alpha and beta portions. It does this simply by sorting them based on their character
-# values. So the output should be the same for a beta-alpha or an alpha-beta from the same fragment.
-AwkScript2='BEGIN {
-  FS = "\t"; OFS = "\t";
-  BAR_LEN = 24;
-  HALF = int(BAR_LEN/2);
-  # Fill an array mapping each character to its ASCII value.
-  for (i = 0; i < 128; i++) {
-    char = sprintf("%c", i);
-    ORD[char] = i;
-  }
-}
-function get_sum(seq) {
-  sum = 0;
-  for (i = 1; i <= length(seq); i++) {
-    sum += ORD[substr(seq, i, 1)];
-  }
-  return sum;
-}
-# Expect first and third column to be the FASTA header, like ">AAAAAAAACCAAAACCATCTCTTA.2".
-length($1) == BAR_LEN + 3 && substr($1, 2, BAR_LEN) == substr($3, 2, BAR_LEN) {
-  alpha = substr($1, 2, HALF);
-  beta = substr($1, HALF+2, HALF);
-  alpha_sum = get_sum(alpha);
-  beta_sum = get_sum(beta);
-  if (alpha_sum < beta_sum) {
-    canonical = alpha beta;
-    order = "ab";
-  } else {
-    canonical = beta alpha;
-    order = "ba";
-  }
-  print canonical, order, $2, $4;
-}'
-
 function main {
   fastq1="$1"
   fastq2="$2"
@@ -54,9 +13,9 @@ function main {
   # command. Mainly, it puts all the data for both read pairs on one line, and adds a column with
   # the barcode.
   # Warning: It assumes the fastq's have 4 lines per read!
-  cat "$fastq1" | paste - - - - \
-    | paste - <(cat "$fastq2" | paste - - - -) \
-    | awk -F '\t' -v OFS='\t' "$AwkScript1" \
+  gunzip -c "$fastq1" | paste - - - - \
+    | paste - <(gunzip -c "$fastq2" | paste - - - -) \
+    | awk -f make-barcodes.awk \
     | sort -k 1 \
     | sscs.py \
     | gzip -c - \
