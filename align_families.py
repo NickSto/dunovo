e1076541a22231028cffb9ea3991a8794a8c0dce
me <nmapsy@gmail.com>
2015-09-28 22:32:29 -0400

    align_families.py: Fix handling of stats, disable logging and --slurm. - fix stats counting to work across multiple processes. - properly count mafft-processed pairs, move counting up a function. - make the workers list just hold the pipes since that's all we need access to. - remove logging support until I make a multiprocess solution. - remove --slurm (user should use --ntasks instead).

--- align_families.py
+++ align_families.py
@@ -3,7 +3,6 @@ from __future__ import division
 import os
 import sys
 import time
-import logging
 import tempfile
 import argparse
 import subprocess
@@ -20,7 +19,6 @@ import seqtools
 
 REQUIRED_COMMANDS = ['mafft']
 OPT_DEFAULTS = {'processes':1}
-USAGE = "%(prog)s [options]"
 DESCRIPTION = """Read in sorted FASTQ data and do multiple sequence alignments of each family."""
 
 
@@ -34,13 +32,8 @@ def main(argv):
          '1. canonical barcode, 2. barcode order ("ab" for alpha+beta, "ba" for beta-alpha) 3. '
          'read 1 name, 4. read 1 sequence, 5. read 1 quality scores, 6. read 2 name, 7. read 2 '
          'sequence, 8. read 2 quality scores.')
-  parser.add_argument('-s', '--stats-file',
-    help='Print statistics on the run to this file. Use "-" to print to stderr.')
   parser.add_argument('-p', '--processes', type=int,
-    help='Number of processes to use. If > 1, launches this many worker subprocesses. '
-         'Default: %(default)s.')
-  parser.add_argument('-S', '--slurm', action='store_true',
-    help='If -p > 1, prepend sub-commands with "srun -C new".')
+    help='Number of worker subprocesses to use. Must be at least 1. Default: %(default)s.')
 
   args = parser.parse_args(argv[1:])
 
@@ -48,8 +41,6 @@ def main(argv):
 
   # Check for required commands.
   missing_commands = []
-  if args.slurm:
-    REQUIRED_COMMANDS.append('srun')
   for command in REQUIRED_COMMANDS:
     if not distutils.spawn.find_executable(command):
       missing_commands.append(command)
@@ -61,22 +52,13 @@ def main(argv):
   else:
     infile = sys.stdin
 
-  if args.stats_file:
-    if args.stats_file == '-':
-      logging.basicConfig(stream=sys.stderr, level=logging.INFO, format='%(message)s')
-    else:
-      logging.basicConfig(filename=args.stats_file, filemode='w', level=logging.INFO,
-                          format='%(message)s')
-  else:
-    logging.disable(logging.CRITICAL)
-
   # Open all the worker processes.
   workers = open_workers(args.processes)
 
   # Main loop.
   # This processes whole duplexes (pairs of strands) at a time for a future option to align the
   # whole duplex at a time.
-  stats = {'duplexes':0, 'time':0, 'pairs':0, 'runs':0}
+  stats = {'duplexes':0, 'time':0, 'pairs':0, 'runs':0, 'aligned_pairs':0}
   current_worker_i = 0
   duplex = collections.OrderedDict()
   family = []
@@ -96,8 +78,8 @@ def main(argv):
       if this_barcode != barcode:
         # sys.stderr.write('processing {}: {} orders ({})\n'.format(barcode, len(duplex),
         #                  '/'.join([str(len(duplex[order])) for order in duplex])))
-        result, current_worker_i = delegate(workers, stats, duplex, barcode)
-        sys.stdout.write(result)
+        output, run_stats, current_worker_i = delegate(workers, stats, duplex, barcode)
+        process_results(output, run_stats, stats)
         duplex = collections.OrderedDict()
       barcode = this_barcode
       order = this_order
@@ -109,8 +91,8 @@ def main(argv):
   duplex[order] = family
   # sys.stderr.write('processing {}: {} orders ({}) [last]\n'.format(barcode, len(duplex),
   #                  '/'.join([str(len(duplex[order])) for order in duplex])))
-  result, current_worker_i = delegate(workers, stats, duplex, barcode)
-  sys.stdout.write(result)
+  output, run_stats, current_worker_i = delegate(workers, stats, duplex, barcode)
+  process_results(output, run_stats, stats)
 
   # Do one last loop through the workers, reading the remaining results and stopping them.
   # Start at the worker after the last one processed by the previous loop.
@@ -118,76 +100,55 @@ def main(argv):
   for i in range(len(workers)):
     worker_i = (start + i) % args.processes
     worker = workers[worker_i]
-    sys.stdout.write(worker['pipe'].recv())
-    worker['pipe'].send(None)
+    output, run_stats = worker.recv()
+    process_results(output, run_stats, stats)
+    worker.send(None)
 
   if infile is not sys.stdin:
     infile.close()
 
-  if not args.stats_file:
-    return
-
   # Final stats on the run.
-  logging.info('Processed {pairs} read pairs in {duplexes} duplexes.'.format(**stats))
-  per_pair = stats['time'] / stats['pairs']
+  sys.stderr.write('Processed {pairs} read pairs in {duplexes} duplexes.\n'.format(**stats))
+  per_pair = stats['time'] / stats['aligned_pairs']
   per_run = stats['time'] / stats['runs']
-  logging.info('{:0.3f}s per pair, {:0.3f}s per run.'.format(per_pair, per_run))
+  sys.stderr.write('{:0.3f}s per pair, {:0.3f}s per run.\n'.format(per_pair, per_run))
 
 
 def open_workers(num_workers):
   """Open the required number of worker processes."""
   workers = []
   for i in range(num_workers):
-<<<<<<< HEAD
-    if args.slurm:
-      command = ['srun', '-C', 'new', 'python', script_path]
-    else:
-      command = ['python', script_path]
-    stats_subfile = None
-    if args.stats_file:
-      if args.stats_file == '-':
-        stats_subfile = '-'
-      else:
-        stats_subfile = "{}.{}.log".format(args.stats_file, i)
-      command.extend(['-s', stats_subfile])
-    outfile = tempfile.NamedTemporaryFile('w', delete=False, prefix='align.msa.')
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=outfile)
-    worker = {'proc':process, 'outfile':outfile, 'stats':stats_subfile}
-=======
-    worker = {}
     parent_pipe, child_pipe = multiprocessing.Pipe()
-    worker['pipe'] = parent_pipe
-    worker['process'] = multiprocessing.Process(target=worker_function, args=(child_pipe,))
-    worker['process'].start()
->>>>>>> align_families.py: First draft of multiprocessing parallel implementation.
-    workers.append(worker)
+    process = multiprocessing.Process(target=worker_function, args=(child_pipe,))
+    process.start()
+    workers.append(parent_pipe)
   return workers
 
 
-def worker_function(pipe):
+def worker_function(child_pipe):
   while True:
-    data = pipe.recv()
-    if data is None:
+    args = child_pipe.recv()
+    if args is None:
       break
-    args, kwargs = data
-    pipe.send(process_duplex(*args, **kwargs))
+    child_pipe.send(process_duplex(*args))
 
 
 def delegate(workers, stats, duplex, barcode):
   stats['duplexes'] += 1
   worker_i = stats['duplexes'] % len(workers)
   worker = workers[worker_i]
-  args = (duplex, barcode)
-  kwargs = {'stats':stats}
   if stats['duplexes'] >= len(workers):
-    result = worker['pipe'].recv()
+    output, run_stats = worker.recv()
   else:
-    result = ''
-  worker['pipe'].send((args, kwargs))
-  return result, worker_i
+    output, run_stats = '', {}
+  args = (duplex, barcode)
+  worker.send(args)
+  return output, run_stats, worker_i
 
 
-def process_duplex(duplex, barcode, stats=None):
+def process_duplex(duplex, barcode):
+  output = ''
+  run_stats = {'time':0, 'runs':0, 'aligned_pairs':0}
   orders = duplex.keys()
   if len(duplex) == 0 or None in duplex:
     return
@@ -202,19 +163,27 @@ def process_duplex(duplex, barcode, stats=None):
     raise AssertionError('Error: More than 2 orders in duplex {}: {}'.format(barcode, orders))
   for mate, order in combos:
     family = duplex[order]
-    alignment = align_family(family, mate, stats)
+    start = time.time()
+    alignment = align_family(family, mate)
+    # Compile statistics.
+    elapsed = time.time() - start
+    pairs = len(family)
+    #logging.info('{} sec for {} read pairs.'.format(elapsed, pairs))
+    if pairs > 1:
+      run_stats['time'] += elapsed
+      run_stats['runs'] += 1
+      run_stats['aligned_pairs'] += pairs
     if alignment is None:
-      #logging.warning('Error aligning family {}/{} (read {}).'.format(barcode, order, mate))
-      return ''
+      pass  #logging.warning('Error aligning family {}/{} (read {}).'.format(barcode, order, mate))
     else:
-      return format_msa(alignment, barcode, order, mate)
+      output += format_msa(alignment, barcode, order, mate)
+  return output, run_stats
 
 
-def align_family(family, mate, stats):
+def align_family(family, mate):
   """Do a multiple sequence alignment of the reads in a family and their quality scores."""
   mate = str(mate)
   assert mate == '1' or mate == '2'
-  start = time.time()
   # Do the multiple sequence alignment.
   seq_alignment = make_msa(family, mate)
   if seq_alignment is None:
@@ -227,13 +196,6 @@ def align_family(family, mate, stats):
   alignment = []
   for aligned_seq, aligned_qual in zip(seq_alignment, qual_alignment):
     alignment.append({'name':aligned_seq['name'], 'seq':aligned_seq['seq'], 'qual':aligned_qual})
-  elapsed = time.time() - start
-  pairs = len(family)
-  #logging.info('{} sec for {} read pairs.'.format(elapsed, pairs))
-  if pairs > 1:
-    stats['time'] += elapsed
-    stats['pairs'] += pairs
-    stats['runs'] += 1
   return alignment
 
 
@@ -299,6 +261,15 @@ def format_msa(align, barcode, order, mate, outfile=sys.stdout):
     output += '{bar}\t{order}\t{mate}\t{name}\t{seq}\t{qual}\n'.format(bar=barcode, order=order,
                                                                        mate=mate, **sequence)
   return output
+
+
+def process_results(output, run_stats, stats):
+  """Process the outcome of a duplex run.
+  Print the aligned output and sum the stats from the run with the running totals."""
+  for key, value in run_stats.items():
+    stats[key] += value
+  if output:
+    sys.stdout.write(output)
 
 
 def fail(message):
