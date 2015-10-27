2144bb11f8e65badfeb8266a64ec51ce66e60c92
me <nmapsy@gmail.com>
2015-10-27 00:47:26 -0400

    loeb-2.0.sh: Add second half of pipeline, document pipeline steps, smarter version checking. - version written Fri Oct 23 23:05:56 2015 -0400. - Separate pipeline into 2 halves: duplex creation and cleanup of the duplex reads.   - Allow running either or both halves. - Label commands with step numbers from Loeb paper, comments from paper. - Add GATK and Picard.

--- loeb-2.0.sh
+++ loeb-2.0.sh
@@ -7,30 +7,57 @@ set -ue
 
 BarcodeLen=${BarcodeLen:=12}
 SpacerLen=${SpacerLen:=5}
+StartClip=${StartClip:=5}
+EndClip=${EndClip:=5}
 BwaCmd=${BwaCmd:="bwa"}
 SamtoolsCmd=${SamtoolsCmd:="samtools"}
 PythonCmd=${PythonCmd:="python"}
-ReadlenDefault=101
+JavaCmd=${JavaCmd:="java"}
+PicardDir=${PicardDir:="$HOME/src/picard-tools-1.100"}
+GatkDir=${GatkDir:="$HOME/src/GenomeAnalysisTK"}
 
-Usage="Usage: \$ $(basename $0) ref.fa reads_1.fq reads_2.fq readlen [outdir]
-Run the Loeb pipeline as it was published in the Kennedy et al. 2014 paper (release 2.0).
-If readlen is not provided, it will assume ${ReadlenDefault}bp.
+Usage="Usage: \$ $(basename $0) [-d|-c|-a] ref.fa reads_1.fq reads_2.fq readlen [outdir]
+Run the Loeb pipeline as it was published in the Kennedy et al. 2014 paper
+(release 2.0). If -d (\"duplex\") is given, it will stop after producing the
+final duplex reads (step 62). This is the default. If -c (\"cleanup\") is given,
+it will skip producing the duplex reads, assuming it's already been done, and
+just do the filtering, realignment, and trimming (steps 63-71). If -a (\"all\")
+is given, it will do the whole pipeline (both halves). If it's not doing the
+second part, Picard and GATK are not required. Otherwise, provide the paths to
+the directories containing their .jars by setting \$PicardDir and \$GatkDir.
 Dependencies:
-Python >= 2.7 (and < 3.0)
-BWA <= 0.6.2
-Samtools <= 0.1.18
+Python    >= 2.7 (and < 3.0)
+BWA       <= 0.6.2
+Samtools  <= 0.1.18
 BioPython 1.62
-PySAM 0.7.5
+PySAM     0.7.5
+Picard    1.107
+GATK      2.4-9
 To just check your dependency versions, run \$ $(basename $0) -v"
 
 function main {
 
   script_dir=$(real_dir)
 
-  if [[ $# == 1 ]] && [[ $1 == '-v' ]]; then
-    print_versions $script_dir
-    exit
-  elif [[ $# -lt 4 ]] || [[ $1 == '-h' ]]; then
+  duplex=true
+  cleanup=''
+  if [[ $# -ge 1 ]]; then
+    if [[ $1 == '-v' ]]; then
+      print_versions $script_dir
+      exit
+    elif [[ $1 == '-d' ]]; then
+      duplex=true
+      cleanup=''
+    elif [[ $1 == '-c' ]]; then
+      duplex=''
+      cleanup=true
+    elif [[ $1 == '-a' ]]; then
+      duplex=true
+      cleanup=true
+    fi
+    shift
+  fi
+  if [[ $# -lt 4 ]] || [[ $1 == '-h' ]]; then
     fail "$Usage"
   else
     ref="$1"
@@ -44,6 +71,13 @@ function main {
     outdir=.
   fi
 
+  if ! echo "$readlen" | grep -qE '^[0-9]+$'; then
+    fail "ERROR: Invalid read length \"$readlen\"."
+  fi
+  if ! [[ -d $outdir ]]; then
+    fail "ERROR: Invalid output directory \"$outdir\"."
+  fi
+
   print_versions $script_dir
 
   echo "
@@ -54,29 +88,90 @@ fastq2:  $fastq2
 readlen: $readlen
 "
 
+  refdict=$(echo "$ref" | sed -E 's/\.fa(sta)?$//').dict
   rlenreal=$((readlen-BarcodeLen-SpacerLen))
+  end_clip_start=$((rlenreal-EndClip+1))
 
-  $PythonCmd $script_dir/tag_to_header.py --infile1 $fastq1 --infile2 $fastq2 \
-    --outfile1 $outdir/read_1.fq.smi --outfile2 $outdir/read_2.fq.smi --barcode_length $BarcodeLen \
-    --spacer_length $SpacerLen
-  $BwaCmd aln $ref $outdir/read_1.fq.smi > $outdir/read_1.aln
-  $BwaCmd aln $ref $outdir/read_2.fq.smi > $outdir/read_2.aln
-  $BwaCmd sampe -s $ref $outdir/read_1.aln $outdir/read_2.aln \
-    $outdir/read_1.fq.smi $outdir/read_2.fq.smi > $outdir/PE_reads.sam
-  $SamtoolsCmd view -Sbu $outdir/PE_reads.sam | $SamtoolsCmd sort - $outdir/PE_reads.sort
-  $PythonCmd $script_dir/ConsensusMaker.py --tagfile $outdir/PE_reads.tagcounts \
-    --infile $outdir/PE_reads.sort.bam --outfile $outdir/SSCS.bam --min 3 --max 1000 --cutoff 0.7 \
-    --Ncutoff 0.3 --readlength $rlenreal --read_type dpm --filt osn
-  $SamtoolsCmd view -bu $outdir/SSCS.bam | $SamtoolsCmd sort - $outdir/SSCS.sort
-  $PythonCmd $script_dir/DuplexMaker.py --infile $outdir/SSCS.sort.bam --outfile $outdir/DCS_data \
-    --Ncutoff 0.3 --readlength $rlenreal
-  $BwaCmd aln $ref $outdir/DCS_data.r1.fq > $outdir/DCS_data.r1.aln
-  $BwaCmd aln $ref $outdir/DCS_data.r2.fq > $outdir/DCS_data.r2.aln
-  $BwaCmd sampe -s $ref $outdir/DCS_data.r1.aln $outdir/DCS_data.r2.aln \
-    $outdir/DCS_data.r1.fq $outdir/DCS_data.r2.fq > $outdir/DCS_PE.aln.sam
-  $SamtoolsCmd view -Sbu $outdir/DCS_PE.aln.sam | $SamtoolsCmd sort - $outdir/DCS_PE.aln.sort
-  $SamtoolsCmd index $outdir/DCS_PE.aln.sort.bam
-  $SamtoolsCmd view -F 4 -b $outdir/DCS_PE.aln.sort.bam > $outdir/DCS_PE.filt.bam
+  if [[ $duplex ]]; then
+    echo '===== 56 ====='
+    # Concatenate the 12-nt tag sequences from the paired reads and evaluate for tag quality
+    $PythonCmd $script_dir/tag_to_header.py --infile1 $fastq1 --infile2 $fastq2 \
+      --outfile1 $outdir/read_1.fq.smi --outfile2 $outdir/read_2.fq.smi \
+      --barcode_length $BarcodeLen --spacer_length $SpacerLen
+    echo '===== 57 ====='
+    # Align each read to the reference genome
+    $BwaCmd aln $ref $outdir/read_1.fq.smi > $outdir/read_1.aln
+    $BwaCmd aln $ref $outdir/read_2.fq.smi > $outdir/read_2.aln
+    echo '===== 58 ====='
+    # Make a single paired-end .sam file
+    $BwaCmd sampe -s $ref $outdir/read_1.aln $outdir/read_2.aln \
+      $outdir/read_1.fq.smi $outdir/read_2.fq.smi > $outdir/PE_reads.sam
+    echo '===== 59 ====='
+    # Convert to .bam format and sort by position
+    $SamtoolsCmd view -Sbu $outdir/PE_reads.sam | $SamtoolsCmd sort - $outdir/PE_reads.sort
+    echo '===== 60 ====='
+    # Run the Python program 'ConsensusMaker.py' to collapse PCR duplicates into SSCS
+    $PythonCmd $script_dir/ConsensusMaker.py --tagfile $outdir/PE_reads.tagcounts \
+      --infile $outdir/PE_reads.sort.bam --outfile $outdir/SSCS.bam --min 3 --max 1000 \
+      --cutoff 0.7 --Ncutoff 0.3 --readlength $rlenreal --read_type dpm --filt osn
+    echo '===== 61 ====='
+    # Sort the SSCS reads
+    $SamtoolsCmd view -bu $outdir/SSCS.bam | $SamtoolsCmd sort - $outdir/SSCS.sort
+    echo '===== 62 ====='
+    # Construct DCSs from SSCSs using 'DuplexMaker.py'
+    $PythonCmd $script_dir/DuplexMaker.py --infile $outdir/SSCS.sort.bam \
+      --outfile $outdir/DCS_data.bam --Ncutoff 0.3 --readlength $rlenreal
+  fi
+
+  if [[ $cleanup ]]; then
+    if ! [[ -s $outdir/DCS_data.r1.fq ]] || ! [[ -s $outdir/DCS_data.r2.fq ]]; then
+      fail "ERROR: Duplex sequences missing! ($outdir/DCS_data.r[12].fq)"
+    fi
+    echo '===== 63 ====='
+    # Align each DCS .fastq to the reference genome
+    $BwaCmd aln $ref $outdir/DCS_data.r1.fq > $outdir/DCS_data.r1.aln
+    $BwaCmd aln $ref $outdir/DCS_data.r2.fq > $outdir/DCS_data.r2.aln
+    echo '===== 64 ====='
+    # Make a paired-end .sam file for the DCS data
+    $BwaCmd sampe -s $ref $outdir/DCS_data.r1.aln $outdir/DCS_data.r2.aln \
+      $outdir/DCS_data.r1.fq $outdir/DCS_data.r2.fq > $outdir/DCS_PE.aln.sam
+    echo '===== 65 ====='
+    # Convert to .bam format and sort by position
+    $SamtoolsCmd view -Sbu $outdir/DCS_PE.aln.sam | $SamtoolsCmd sort - $outdir/DCS_PE.aln.sort
+    echo '===== 66 ====='
+    # Index the final sorted DCS .bam file
+    $SamtoolsCmd index $outdir/DCS_PE.aln.sort.bam
+    echo '===== 67 ====='
+    # Filter out unmapped reads from the final DCS .bam file
+    $SamtoolsCmd view -F 4 -b $outdir/DCS_PE.aln.sort.bam > $outdir/DCS_PE.filt.bam
+    echo '===== 68 ====='
+    # Add readgroups field to the header of the final DCS .bam file with Picard to allow for
+    # compatibility with the GATK using Picard tools.
+    $JavaCmd -jar -Xmx2g $PicardDir/AddOrReplaceReadGroups.jar INPUT=$outdir/DCS_PE.filt.bam \
+      OUTPUT=$outdir/DCS_PE.filt.readgroups.bam RGLB=UW RGPL=Illumina RGPU=ATATAT RGSM=default
+    echo '===== 69 ====='
+    # Index the final sorted DCS .bam file
+    $SamtoolsCmd index $outdir/DCS_PE.filt.readgroups.bam
+    echo '===== 70 ====='
+    if ! [[ -s $refdict ]]; then
+      rm -f $refdict
+      $JavaCmd -jar $PicardDir/CreateSequenceDictionary.jar REFERENCE=$ref OUTPUT=$refdict
+    fi
+    # Perform local re-alignment of the reads using GATK. First identify the genome targets for
+    # local re-alignment.
+    $JavaCmd -Xmx2g -jar $GatkDir/GenomeAnalysisTK.jar -T RealignerTargetCreator -R $ref \
+      -I $outdir/DCS_PE.filt.readgroups.bam -o $outdir/DCS_PE.filt.readgroups.intervals
+    # This command is followed by the actual local re-alignment.
+    $JavaCmd -Xmx2g -jar $GatkDir/GenomeAnalysisTK.jar -T IndelRealigner -R $ref \
+      -I $outdir/DCS_PE.filt.readgroups.bam -o $outdir/DCS_PE.filt.readgroups.realign.bam \
+      -targetIntervals $outdir/DCS_PE.filt.readgroups.intervals
+    echo '===== 71 ====='
+    # Perform end-trimming of DCS reads. An example command that trims five bases from both the 3'
+    # and 5' ends of each read is provided.
+    $JavaCmd -Xmx2g -jar $GatkDir/GenomeAnalysisTK.jar -T ClipReads \
+      -I $outdir/DCS_PE.filt.readgroups.realign.bam -o $outdir/DCS-final.bam -R $ref \
+      --cyclesToTrim "1-$StartClip,$end_clip_start-$rlenreal" --clipRepresentation SOFTCLIP_BASES
+  fi
 }
 
 
@@ -111,21 +206,96 @@ function real_dir {
 function print_versions {
   script_dir="$1"
   echo -e 'VERSIONS\trecommended\tpresent'
+  # pipeline
   echo -en 'pipeline:\te0897da\t\t'
-  unset CDPATH
-  cd $script_dir
-  git log --oneline -n 1 | grep --color=never -Eo '^\S+'
-  cd - >/dev/null
+  if ! [[ -d $script_dir ]]; then
+    echo 'MISSING'
+  elif ! which git >/dev/null 2>/dev/null; then
+    echo 'ERROR 1'
+  else
+    unset CDPATH
+    cd $script_dir
+    if ! git log >/dev/null 2>/dev/null; then
+      echo 'ERROR 2'
+    else
+      git log --oneline -n 1 | grep --color=never -Eo '^\S+'
+    fi
+    cd - >/dev/null
+  fi
+  # Python
   echo -en 'Python:\t\t2.7\t\t'
-  $PythonCmd --version 2>&1 | sed -E 's/python\s//I'
+  if which $PythonCmd >/dev/null 2>/dev/null; then
+    $PythonCmd --version 2>&1 | sed -E 's/python\s//I'
+  else
+    echo 'MISSING'
+  fi
+  # BWA
   echo -en 'BWA:\t\t0.6.2\t\t'
-  $BwaCmd 2>&1 | sed -En 's/^.*version.*\s([0-9].*)$/\1/Ip'
+  if which $BwaCmd >/dev/null 2>/dev/null; then
+    $BwaCmd 2>&1 | sed -En 's/^.*version.*\s([0-9].*)$/\1/Ip'
+  else
+    echo 'MISSING'
+  fi
+  # Samtools
   echo -en 'Samtools:\t0.1.18\t\t'
-  $SamtoolsCmd 2>&1 | sed -En 's/^.*version.*\s([0-9].*)$/\1/Ip'
+  if which $SamtoolsCmd >/dev/null 2>/dev/null; then
+    $SamtoolsCmd 2>&1 | sed -En 's/^.*version.*\s([0-9].*)$/\1/Ip'
+  else
+    echo 'MISSING'
+  fi
+  # PySAM
   echo -en 'PySAM:\t\t0.7.5\t\t'
-  $PythonCmd -c 'import pysam; print pysam.__version__'
+  if $PythonCmd -c 'import pysam' 2>/dev/null; then
+    $PythonCmd -c 'import pysam; print pysam.__version__'
+  elif which $PythonCmd >/dev/null 2>/dev/null; then
+    echo 'MISSING'
+  else
+    echo 'ERROR 1'
+  fi
+  # BioPython
   echo -en 'BioPython:\t1.62\t\t'
-  $PythonCmd -c 'import Bio; print Bio.__version__'
+  if $PythonCmd -c 'import Bio' 2>/dev/null; then
+    $PythonCmd -c 'import Bio; print Bio.__version__'
+  elif which $PythonCmd >/dev/null 2>/dev/null; then
+    echo 'MISSING'
+  else
+    echo 'ERROR 1'
+  fi
+  if ! which $JavaCmd 2>/dev/null >/dev/null; then
+    echo "ERROR: Java command \"$JavaCmd\" not found." >&2
+    return
+  fi
+  # Picard
+  echo -en 'Picard:\t\t1.107\t\t'
+  if [[ -f $PicardDir/picard.jar ]]; then
+    $JavaCmd -jar $PicardDir/picard.jar AddOrReplaceReadGroups --version 2>&1 >/dev/null | sed -E 's/\(.*\)//'
+  elif ! [[ -f $PicardDir/AddOrReplaceReadGroups.jar ]]; then
+    echo 'MISSING'
+  elif [[ $($JavaCmd -jar $PicardDir/AddOrReplaceReadGroups.jar 2>&1 >/dev/null | sed -En 's/^Version:?\s//ip') ]]; then
+    $JavaCmd -jar $PicardDir/AddOrReplaceReadGroups.jar 2>&1 >/dev/null | sed -En 's/^Version:?\s//ip'
+  else
+    echo 'ERROR 1'
+  fi
+  # GATK
+  echo -en 'GATK:\t\t2.4-9\t\t'
+  if ! [[ -f $GatkDir/GenomeAnalysisTK.jar ]]; then
+    echo 'MISSING'
+  else
+    set +e
+    version=$($JavaCmd -jar $GatkDir/GenomeAnalysisTK.jar --version 2>/dev/null)
+    exit_code=$?
+    set -e
+    if [[ $exit_code == 0 ]]; then
+      echo $version
+    else
+      version=$($JavaCmd -jar $GatkDir/GenomeAnalysisTK.jar 2>&1 >/dev/null | sed -En 's/^.*version\s([0-9.-]+[0-9.]).*$/\1/p')
+      if [[ $version ]]; then
+        echo $version
+      else
+        echo 'ERROR 1'
+      fi
+    fi
+  fi
 }
 
 
