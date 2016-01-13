c8f922f0fe2678725ec7ebfd19a67e067f227f1d
me <nmapsy@gmail.com>
2016-01-13 12:44:31 -0500

    LICENSE.txt, swalignc.c: Clarify license of swalign code.

--- swalignc.c
+++ swalignc.c
@@ -22,6 +22,7 @@
  *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  *  OTHER DEALINGS IN THE SOFTWARE.
  */
+// Note: That's an MIT license.
 // All double-commented comments below are from Nicolaus Lance Hepler.
 
 #include "swalignc.h"
