90f08d5ef9974be490629c7fab00161b8695bd58
me <nmapsy@gmail.com>
2016-04-07 12:18:09 -0700

    utils/sim.py: First working version that adds PCR errors.

--- utils/sim.py
+++ utils/sim.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 import os
 import sys
+import copy
 import numpy
 import bisect
 import random
@@ -36,11 +37,14 @@ def main(argv):
 
   parser.add_argument('ref', metavar='ref.fa', nargs='?',
     help='Reference sequence.')
+  parser.add_argument('-n', '--n-frags', type=int,
+    help='The number of original fragment molecules to simulate. The final number of reads will be '
+         'this multiplied by the average number of reads per family. If you provide fragments with '
+         '--frag-file, the script will still only read in the number specified here. Default: '
+         '%(default)s')
   parser.add_argument('-r', '--read-len', type=int,
     help='Default: %(default)s')
   parser.add_argument('-f', '--frag-len', type=int,
-    help='Default: %(default)s')
-  parser.add_argument('-F', '--n-frags', type=int,
     help='Default: %(default)s')
   parser.add_argument('-s', '--seq-error', type=float,
     help='Sequencing error rate per base (0-1 proportion, not percent). Default: %(default)s')
@@ -65,18 +69,18 @@ def main(argv):
   # Overview: wgsim "reads" (fragments) from the reference with 0 mutation rate, then wgsim actual
   # duplex reads from each fragment.
 
-  #TODO: Check for wgsim on the PATH.
-
   # Create a temporary director to do our work in. Then work inside a try so we can finally remove
   # the directory no matter what exceptions are encountered.
-  tmpdir = tempfile.mkdtemp(prefix='wgdsim.')
+  tmpfile = tempfile.NamedTemporaryFile(prefix='wgdsim.frags.')
+  tmpfile.close()
   try:
     # Step 1: Use wgsim to create fragments from the reference.
     if args.frag_file:
       frag_file = args.frag_file
     else:
-      frag_file = os.path.join(tmpdir, 'fragments.fq')
+      frag_file = tmpfile.name
       #TODO: Check exit status
+      #TODO: Check for wgsim on the PATH.
       run_command('wgsim', '-e', '0', '-r', '0', '-d', '0', '-R', args.indel_rate, '-N', args.n_frags,
                   '-X', args.extension_rate, '-1', args.frag_len, args.ref, frag_file, os.devnull)
 
@@ -84,15 +88,17 @@ def main(argv):
     count = 0
     extended_dist = extend_dist(RAW_DISTRIBUTION)
     proportional_dist = compile_dist(extended_dist)
+    n_frags = 0
     for fragment in fastqreader.FastqReadGenerator(frag_file):
-      # print('>'+fragment.name, fragment.seq, '+', fragment.qual, sep='\n')
-      read1_orig = fragment.seq[:args.read_len]
+      n_frags += 1
+      if n_frags > args.n_frags:
+        break
       # Step 2: Determine how many reads to produce from each fragment.
       # - Use random.random() and divide the range 0-1 into segments of sizes proportional to
       #   the likelihood of each family size.
       # bisect.bisect() finds where an element belongs in a sorted list, returning the index.
       # proportional_dist is just such a sorted list, with values from 0 to 1.
-      nreads = bisect.bisect(proportional_dist, random.random())
+      n_reads = bisect.bisect(proportional_dist, random.random())
 
       # Step 3: Introduce PCR errors.
       # - Determine the mutations and their frequencies.
@@ -100,29 +106,45 @@ def main(argv):
       #     - Important to have PCR errors shared between reads.
       # - For each read, determine which mutations it contains.
       #   - Use random.random() < mut_freq.
+      tree = get_one_pcr_tree(n_reads, args.cycles, 1000)
+      # Add errors to all children of original fragment.
+      subtree1 = tree.get('child1')
+      subtree2 = tree.get('child2')
+      add_pcr_errors(subtree1, args.read_len, args.pcr_error, args.indel_rate, args.extension_rate)
+      add_pcr_errors(subtree2, args.read_len, args.pcr_error, args.indel_rate, args.extension_rate)
+      apply_pcr_errors(tree, fragment.seq)
+      reads = get_final_fragments(tree)
 
       # Step 4: Introduce sequencing errors.
-      read1s = []
-      for i in range(nreads):
-        read1 = read1_orig
+      for read_i in range(len(reads)):
+        read = reads[read_i]
         for mutation in generate_mutations(args.read_len, args.seq_error, args.indel_rate,
                                            args.extension_rate):
           #TODO: Note that an indel earlier in the read will mean later indels won't be inserted at
           #      the originally intended coordinate. Fix?
-          read1 = apply_mutation(mutation, read1)
-        read1s.append(read1)
+          read = apply_mutation(mutation, read)
+        reads[read_i] = read[:args.read_len]
       #TODO: Add barcodes and invariant sequences.
 
       # Print family.
-      for read1 in read1s:
+      for read in reads:
         count += 1
-        print('>'+str(count), read1, '+', 'I' * len(read1), sep='\n')
+        print('>'+str(count), read, '+', 'I' * len(read), sep='\n')
 
   finally:
-    shutil.rmtree(tmpdir)
+    try:
+      os.remove(tmpfile.name)
+    except OSError:
+      pass
 
 
-def run_command(*command):
+def run_command(*command, **kwargs):
+  """Run a command:
+  run_command('echo', 'hello')
+  Will print the command to stderr before running, unless "silent" is set to True."""
+  command_strs = map(str, command)
+  if not kwargs.get('silent'):
+    sys.stderr.write('$ '+' '.join(command_strs)+'\n')
   devnull = open(os.devnull, 'w')
   try:
     exit_status = subprocess.call(map(str, command), stderr=devnull)
@@ -161,35 +183,6 @@ def compile_dist(raw_dist):
     current_sum += magnitude
     proportional_dist.append(current_sum/final_sum)
   return proportional_dist
-
-
-#TODO: Rewrite.
-"""No more simulating every mutation and whether it affected one of the final reads.
-Instead, just take each of the final reads and ask whether a mutation occurred in any one of its
-ancestors. But unfortunately we can't just step through each previous cycle and generate errors.
-What we care about is errors shared between reads. To address that, at each cycle we have to ask
-whether the read shares that ancestor with any of the other reads. Then we have to propagate each of
-those errors to all shared reads. Basically, we need to work backwards, building a tree of how each
-of the reads is related. Essentially, coalescent theory.
-Here's the plan:
-The tree will be composed of nested lists of lists, as suggested here:
-https://stackoverflow.com/questions/3009935/looking-for-a-good-python-tree-data-structure
-We'll go through the reads, and for each one, build a chain upward. At each cycle, we'll ask whether
-this ancestor is shared with any of the other reads (Can use numpy.random.binomial(num_reads, p)
-where p = 1/2**cycle (the probability of sharing an ancestor at that level). If it returns 1 or
-greater, then we randomly choose a different read and join their chains.)
-After building the tree, we go back down it, starting from the root, simulating mutations. Can use
-recursion to propagate a mutation down both branches when they occur. Do this for every base.
-"""
-def make_pcr_errors(cycles, error_rate, read_len, n_reads, min_prob):
-  # For each base..
-  for i in range(read_len):
-    # For each cycle..
-    for cycle in range(1, cycles+1):
-      replications = 2**cycle
-      if n_reads * 1/replications < min_prob:
-        continue
-      n_muts = numpy.random.binomial(replications, error_rate)
 
 
 def generate_mutations(read_len, error_rate, indel_rate, extension_rate):
@@ -247,6 +240,255 @@ def apply_mutation(mut, seq):
 
 def get_rand_base():
   return random.choice(('A', 'C', 'G', 'T'))
+
+
+def add_pcr_errors(subtree, read_len, error_rate, indel_rate, extension_rate):
+  """Add simulated PCR errors to a node in a tree and all its descendants."""
+  # Note: The errors are intended as "errors made in creating this molecule", so don't apply this to
+  # the root node, since that is supposed to be the original, unaltered molecule.
+  # Go down the subtree and simulate errors in creating each fragment.
+  # Process all the first-child descendants of the original node in a loop, and recursively call
+  # this function to process all second children.
+  node = subtree
+  while node:
+    node['errors'] = list(generate_mutations(read_len, error_rate, indel_rate, extension_rate))
+    add_pcr_errors(node.get('child2'), read_len, error_rate, indel_rate, extension_rate)
+    node = node.get('child1')
+
+
+def apply_pcr_errors(subtree, seq):
+  node = subtree
+  while node:
+    for error in node.get('errors', ()):
+      seq = apply_mutation(error, seq)
+    if 'child1' not in node:
+      node['seq'] = seq
+    apply_pcr_errors(node.get('child2'), seq)
+    node = node.get('child1')
+
+
+def get_final_fragments(tree):
+  fragments = []
+  nodes = [tree]
+  while nodes:
+    node = nodes.pop()
+    child1 = node.get('child1')
+    if child1:
+      nodes.append(child1)
+    else:
+      fragments.append(node['seq'])
+    child2 = node.get('child2')
+    if child2:
+      nodes.append(child2)
+  return fragments
+
+
+def get_one_pcr_tree(n_reads, n_cycles, max_tries):
+  """Return a single PCR tree from build_pcr_tree(), or fail if one cannot be found in max_tries.
+  Compensate for the bug in build_pcr_tree() that results in multiple trees sometimes."""
+  trees = []
+  tries = 0
+  while len(trees) != 1:
+    trees = build_pcr_tree(n_reads, n_cycles)
+    tries += 1
+    assert tries < max_tries, 'Could not generate a single, unified tree! (tried {} times)'.format(
+                              max_tries)
+  return trees[0]
+
+
+def build_pcr_tree(n_reads, n_cycles):
+  """Create a simulated descent lineage of how all the PCR reads are related.
+  Each node represents a fragment molecule at one stage of PCR. Each node is a dict containing the
+  fragment's children (other nodes) ('child1' and 'child2') and the PCR cycle number ('cycle').
+  Returns a list of root nodes. Usually there will only be one, but about 1-3% of the time it fails
+  to unify the subtrees and results in a broken tree.
+  """
+  #TODO: Make it always return a single tree.
+  # Begin a branch for each of the reads. These are the leaf nodes. We'll work backward from these,
+  # simulating the points at which they share ancestors, eventually coalescing into the single
+  # (root) ancestor.
+  branches = []
+  for read in range(n_reads):
+    branches.append({'cycle':n_cycles-1})
+  # Build up all the branches in parallel. Start from the second-to-last PCR cycle.
+  for cycle in reversed(range(n_cycles-1)):
+    # Probability of 2 reads sharing an ancestor at cycle c is 1/2^c.
+    prob = 1/2**cycle
+    read_i = 0
+    while read_i < len(branches):
+      current_root = branches[read_i]
+      # Does the current read share this ancestor with any of the other reads?
+      # numpy.random.binomial() is a fast way to simulate going through every other read and
+      # asking if random.random() < prob.
+      shared = numpy.random.binomial(len(branches)-1, prob)
+      if shared == 0:
+        # No branch point here. Just add another level to the lineage.
+        branches[read_i] = {'cycle':cycle, 'child1':current_root}
+      else:
+        # Pick a random other read to share this ancestor with.
+        # Make a list of candidates to pick from.
+        candidates = []
+        for candidate_i, candidate in enumerate(branches):
+          # Don't include ourselves.
+          if candidate is current_root:
+            continue
+          # If it's at a cycle above us and it already has a child, skip it.
+          if candidate['cycle'] == cycle and candidate.get('child2'):
+            continue
+          candidates.append(candidate_i)
+        if candidates:
+          relative_i = random.choice(candidates)
+          relative = branches[relative_i]
+          # Have we already passed this read on this cycle?
+          if relative['cycle'] == cycle:
+            # If we've already passed it, we're looking at the read's parent. We want the child.
+            relative = relative['child1']
+          # Join the lineages of our current read and the relative to a new parent.
+          #TODO: Sometimes, we end up matching up subtrees of different depths. But the discrepancy
+          #      is rarely greater than 1. Figure out why.
+          assert current_root['cycle'] - relative['cycle'] < 3, ('cycle: {}, current_root: {}, '
+            'relative: {}, read_i: {}, relative_i: {}, branches: {}, candidates: {}, shared: {}'
+            .format(cycle, current_root['cycle'], relative['cycle'], read_i, relative_i,
+                    len(branches), len(candidates), shared))
+          branches[read_i] = {'cycle':cycle, 'child1':current_root, 'child2':relative}
+          # Remove the relative from the list of lineages.
+          del(branches[relative_i])
+          if relative_i < read_i:
+            read_i -= 1
+      read_i += 1
+  return branches
+
+
+def get_depth(tree):
+  depth = 0
+  node = tree
+  while node:
+    depth += 1
+    node = node.get('child1')
+  return depth
+
+
+def convert_tree(tree_orig):
+  # Let's operate on a copy only.
+  tree = copy.deepcopy(tree_orig)
+  # Turn the tree vertical.
+  tree['line'] = 1
+  tree['children'] = 0
+  levels = [[tree]]
+  done = False
+  while not done:
+    last_level = levels[-1]
+    this_level = []
+    done = True
+    for node in last_level:
+      for child_name in ('child1', 'child2'):
+        child = node.get(child_name)
+        if child:
+          done = False
+          child['parent'] = node
+          child['branch'] = child['parent']['branch']
+          if child_name == 'child2':
+            child['branch'] += 1
+          this_level.append(child)
+    this_level.sort(key=lambda node: node['branch'])
+    levels.append(this_level)
+  return levels
+
+
+def print_levels(levels):
+  last_level = []
+  for level in levels:
+    for node in level:
+      child = 1
+      for parent in last_level:
+        if parent.get('child2') is node:
+          child = 2
+      if child == 1:
+        sys.stdout.write('| ')
+      else:
+        sys.stdout.write('\ ')
+    last_level = level
+    print()
+
+
+def label_branches(tree):
+  """Label each vertical branch (line of 'child1's) with an id number."""
+  counter = 1
+  tree['branch'] = counter
+  nodes = [tree]
+  while nodes:
+    node = nodes.pop(0)
+    child1 = node.get('child1')
+    if child1:
+      child1['branch'] = node['branch']
+      nodes.append(child1)
+    child2 = node.get('child2')
+    if child2:
+      counter += 1
+      child2['branch'] = counter
+      nodes.append(child2)
+
+
+def print_tree(tree_orig):
+  # We "write" strings to an output buffer instead of directly printing, so we can post-process the
+  # output. The buffer is a matrix of cells, each holding a string representing one element.
+  lines = [[]]
+  # Let's operate on a copy only.
+  tree = copy.deepcopy(tree_orig)
+  # Add some bookkeeping data.
+  label_branches(tree)
+  tree['level'] = 0
+  branches = [tree]
+  while branches:
+    line = lines[-1]
+    branch = branches.pop()
+    level = branch['level']
+    while level > 0:
+      line.append('  ')
+      level -= 1
+    node = branch
+    while node:
+      # Is it the root node? (Have we written anything yet?)
+      if lines[0]:
+        # Are we at the start of the line? (Is it only spaces so far?)
+        if line[-1] == '  ':
+          line.append('\-')
+        elif line[-1].endswith('-'):
+          line.append('=-')
+      else:
+        line.append('*-')
+      child2 = node.get('child2')
+      if child2:
+        child2['level'] = node['level'] + 1
+        branches.append(child2)
+      parent = node
+      node = node.get('child1')
+      if node:
+        node['level'] = parent['level'] + 1
+      else:
+        line.append(' {}'.format(parent['branch']))
+        lines.append([])
+  # Post-process output: Add lines connecting branches to parents.
+  x = 0
+  done = False
+  while not done:
+    # Draw vertical lines upward from branch points.
+    drawing = False
+    for line in reversed(lines):
+      done = True
+      if x < len(line):
+        done = False
+        cell = line[x]
+        if cell == '\-':
+          drawing = True
+        elif cell == '  ' and drawing:
+          line[x] = '| '
+        elif cell == '=-' and drawing:
+          drawing = False
+    x += 1
+  # Print the final output.
+  for line in lines:
+    print(''.join(line))
 
 
 def fail(message):
