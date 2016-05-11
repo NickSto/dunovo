845b4c26bca11bf142222acfade0d4619f0120cb
me <nmapsy@gmail.com>
2016-05-10 22:48:04 -0400

    correct.py: Fix bugs, add sequence data to groups, add debug printing.

--- correct.py
+++ correct.py
@@ -2,9 +2,11 @@
 from __future__ import division
 from __future__ import print_function
 import sys
+import logging
 import argparse
 
-ARG_DEFAULTS = {'sam':sys.stdin, 'qual':20, 'pos':2, 'dist':1}
+ARG_DEFAULTS = {'sam':sys.stdin, 'qual':20, 'pos':2, 'dist':1, 'log':sys.stderr,
+                'log_level':logging.ERROR}
 USAGE = "%(prog)s [options]"
 DESCRIPTION = """"""
 
@@ -23,11 +25,19 @@ def main(argv):
   parser.add_argument('-p', '--pos', type=int,
     help='POS tolerance. Alignments will be ignored if abs(POS - 1) is greater than this value. '
          'Set to greater than the barcode length for no threshold. Default: %(default)s')
+  parser.add_argument('-l', '--log', type=argparse.FileType('w'),
+    help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
+  parser.add_argument('-D', '--debug', dest='log_level', action='store_const', const=logging.DEBUG,
+    help='Print debug messages (very verbose).')
 
   args = parser.parse_args(argv[1:])
 
+  logging.basicConfig(stream=args.log, level=args.log_level, format='%(message)s')
+  tone_down_logger()
+
   # Group reads (barcodes) into sets of reads linked by alignments to each other.
-  # Each group of reads is a set() of read names. "groups" is a list of these sets.
+  # Each group of reads is a dict mapping read names to read sequences. "groups" is a list of these
+  # dicts.
   groups = []
   # "member_to_group" is a dict mapping read names to their group's index in "groups".
   member_to_group = {}
@@ -39,9 +49,7 @@ def main(argv):
     fields = line.split('\t')
     read_name = fields[0]
     ref_name = fields[2]
-    # If the barcode is aligned to itself, we don't care about that.
-    if read_name == ref_name:
-      continue
+    read_seq = fields[9]
     # Apply alignment quality filters.
     try:
       flags = int(fields[1])
@@ -62,55 +70,90 @@ def main(argv):
         try:
           nm = int(tag[5:])
         except ValueError:
-          sys.stderr.write('Invalid NM tag "{}" on line {}\n.'.format(tag, line_num))
+          logging.error('Invalid NM tag "{}" on line {}.'.format(tag, line_num))
           raise
         break
     assert nm is not None, line_num
     if nm > args.dist:
       continue
+    logging.debug('Ref {}, read {}:'.format(ref_name, read_name))
     # It's a good alignment. Add it to a group.
     group_i_ref = member_to_group.get(ref_name)
     group_i_read = member_to_group.get(read_name)
     if group_i_ref is None:
       group_i = group_i_read
+      logging.debug('\tgroup_i_ref  is None, using {}.'.format(group_i_read))
     elif group_i_read is None:
       group_i = group_i_ref
+      logging.debug('\tgroup_i_read is None, using {}.'.format(group_i_ref))
     elif group_i_ref == group_i_read:
       group_i = group_i_ref
+      logging.debug('\tgroup_i_ref == group_i_read ({}).'.format(group_i_ref))
     else:
       # Both the read and ref are already in a group, but they're different groups. We need to join
       # them.
+      logging.debug('\tgroup_i_ref  is {}, group_i_read is {}.'.format(group_i_ref, group_i_read))
+      logging.debug('\tJoining groups {} and {}.'.format(group_i_ref, group_i_read))
       join_groups(groups, member_to_group, ref_name, read_name)
       group_i = member_to_group[ref_name]
     if group_i is None:
-      new_group = set((ref_name, read_name))
+      # No group exists yet. Create one and add it.
+      new_group = {ref_name:None, read_name:read_seq}
       groups.append(new_group)
       group_i = len(groups) - 1
       member_to_group[ref_name] = group_i
       member_to_group[read_name] = group_i
+      logging.debug('\tAdding new group ({}): {} and {}.'.format(group_i, ref_name, read_name))
     else:
+      # Add these reads to the group.
+      logging.debug('\tAdding {} and {} to group {}.'.format(ref_name, read_name, group_i))
       group = groups[group_i]
-      group.add(ref_name)
-      group.add(read_name)
+      if not group.get(ref_name):
+        logging.debug('\t\t{} not in {}; adding it.'.format(ref_name, group_i))
+        group[ref_name] = None
+        member_to_group[ref_name] = group_i
+      else:
+        logging.debug('\t\t{} already in {}.'.format(ref_name, group_i))
+      if not group.get(read_name):
+        logging.debug('\t\t{} not in {}; adding it.'.format(read_name, group_i))
+        group[read_name] = read_seq
+        member_to_group[read_name] = group_i
+      else:
+        logging.debug('\t\t{} already in {}.'.format(read_name, group_i))
+
+  for i, group in enumerate(groups):
+    print('{}:'.format(i))
+    if group:
+      for name, seq in group.items():
+        print('\t{}:\t{}'.format(name, seq))
 
 
 def join_groups(groups, member_to_group, ref_name, read_name):
   group_i_ref = member_to_group[ref_name]
   group_i_read = member_to_group[read_name]
+  logging.debug('\t\tmember_to_group[{}] == {}'.format(ref_name, group_i_ref))
+  logging.debug('\t\tmember_to_group[{}] == {}'.format(read_name, group_i_read))
   group_ref = groups[group_i_ref]
   group_read = groups[group_i_read]
   # Get a union of all elements in both groups.
-  group_union = group_ref.union(group_read)
+  group_union = group_ref
+  for name, seq in group_read.items():
+    # Only update the read if its name isn't present or its sequence is None.
+    if not group_union.get(name):
+      group_union[name] = seq
   # Put the new, union group back in place of the ref group, and make that the canonical group.
   groups[group_i_ref] = group_union
   groups[group_i_read] = None
-  member_to_group[ref_name] = group_i_ref
-  member_to_group[read_name] = group_i_ref
+  for name in group_union:
+    member_to_group[name] = group_i_ref
 
 
-def fail(message):
-  sys.stderr.write(message+"\n")
-  sys.exit(1)
+def tone_down_logger():
+  """Change the logging level names from all-caps to capitalized lowercase.
+  E.g. "WARNING" -> "Warning" (turn down the volume a bit in your log files)"""
+  for level in (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG):
+    level_name = logging.getLevelName(level)
+    logging.addLevelName(level, level_name.capitalize())
 
 
 if __name__ == '__main__':
