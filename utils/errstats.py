390bd361054aaddd663a677c94436dc266c10114
Nick <nmapsy@gmail.com>
2017-03-21 11:13:06 -0700

    utils/errstats.py: Add option to output counts of occurrences for all repeat errors.

--- utils/errstats.py
+++ utils/errstats.py
@@ -8,8 +8,12 @@ import errno
 import logging
 import argparse
 
-ARG_DEFAULTS = {'infile':sys.stdin, 'log':sys.stderr, 'volume':logging.ERROR}
-DESCRIPTION = """"""
+ARG_DEFAULTS = {'input':sys.stdin, 'log':sys.stderr, 'volume':logging.ERROR}
+DESCRIPTION = """Tally statistics on errors in reads, compared to the rest of their (single-
+stranded) families. Output columns without --all-repeats: 1. barcode, 2. order, 3. mate, 4. number
+of errors that occurred more than once, 5-end. number of errors in each read. With --all-repeats:
+1. barcode, 2. order, 3. mate, 4. number of reads, 5-end. count of occurrence of each error that
+occurred more than once."""
 
 
 def make_argparser():
@@ -17,10 +21,10 @@ def make_argparser():
   parser = argparse.ArgumentParser(description=DESCRIPTION)
   parser.set_defaults(**ARG_DEFAULTS)
 
-  parser.add_argument('infile', metavar='families.msa.tsv', nargs='?', type=argparse.FileType('r'),
+  parser.add_argument('input', metavar='families.msa.tsv', nargs='?', type=argparse.FileType('r'),
     help='')
   parser.add_argument('-H', '--human', action='store_true')
-  parser.add_argument('-S', '--status-file', type=argparse.FileType('w'))
+  parser.add_argument('-r', '--all-repeats', action='store_true')
   parser.add_argument('-l', '--log', type=argparse.FileType('w'),
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
   parser.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL)
@@ -38,20 +42,27 @@ def main(argv):
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
   tone_down_logger()
 
-  run(args.infile, human=args.human, status_file=args.status_file)
+  run(args.input, **vars(args))
 
 
-def run(infile, human=False, status_file=None):
+def run(infile, human=False, all_repeats=False, *nargs, **kwargs):
   for family in parse(infile):
     for order in ('ab', 'ba'):
       for mate in (0, 1):
         alignment = family[order][mate]
         if alignment:
-          consensus, errors, repeat_errors, new_align = compare(alignment, human)
+          consensus, errors, repeat_errors, new_align = compare(alignment, all_repeats=all_repeats,
+                                                                make_new_align=human)
           if human:
             for seq, seq_errors in zip(new_align, errors):
               print('{} errors: {}'.format(seq, seq_errors))
-            print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors), repeat_errors))
+            if all_repeats:
+              print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors),
+                                                                ', '.join(map(str, repeat_errors))))
+            else:
+              print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors), repeat_errors))
+          elif all_repeats:
+            print(family['bar'], order, mate, len(alignment), *repeat_errors, sep='\t')
           else:
             print(family['bar'], order, mate, repeat_errors, *errors, sep='\t')
 
@@ -74,12 +85,15 @@ def parse(infile):
   yield family
 
 
-def compare(alignment, make_new_align=False):
+def compare(alignment, all_repeats=False, make_new_align=False):
   num_seqs = len(alignment)
   consensus = ''
   new_align = [''] * num_seqs
   errors = [0] * num_seqs
-  repeat_errors = 0
+  if all_repeats:
+    repeat_errors = []
+  else:
+    repeat_errors = 0
   for bases in zip(*alignment):
     # Tally how many of each base there are at this position, and find the "winner" (the consensus).
     votes = {'A':0, 'C':0, 'G':0, 'T':0, '-':0, 'N':0}
@@ -104,7 +118,10 @@ def compare(alignment, make_new_align=False):
     # How often do we see the same error more than once?
     for base, vote in votes.items():
       if base != cons and vote > 1:
-        repeat_errors += 1
+        if all_repeats:
+          repeat_errors.append(vote)
+        else:
+          repeat_errors += 1
     consensus += cons
     i += 1
   return consensus, errors, repeat_errors, new_align
