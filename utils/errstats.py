2bfb4250f7c5338b289025b11ef3e44eba38bca7
Nick <nmapsy@gmail.com>
2017-04-06 23:29:57 -0400

    utils/errstats.py: Consider quality scores in consensus making and error counting.

--- utils/errstats.py
+++ utils/errstats.py
@@ -8,7 +8,8 @@ import errno
 import logging
 import argparse
 
-ARG_DEFAULTS = {'input':sys.stdin, 'log':sys.stderr, 'volume':logging.ERROR}
+ARG_DEFAULTS = {'input':sys.stdin, 'qual_thres':0, 'qual_format':'sanger', 'log':sys.stderr,
+                'volume':logging.ERROR}
 DESCRIPTION = """Tally statistics on errors in reads, compared to the rest of their (single-
 stranded) families.
 Output columns without --all-repeats:
@@ -35,9 +36,12 @@ def make_argparser():
     help='')
   parser.add_argument('-H', '--human', action='store_true')
   parser.add_argument('-r', '--all-repeats', action='store_true')
+  parser.add_argument('-q', '--qual-thres', type=int)
+  parser.add_argument('-F', '--qual-format', choices=('sanger', 'solexa', 'illumina1.3',
+                                                      'illumina1.5', 'illumina1.8'))
   parser.add_argument('-l', '--log', type=argparse.FileType('w'),
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
-  parser.add_argument('-q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL)
+  parser.add_argument('-Q', '--quiet', dest='volume', action='store_const', const=logging.CRITICAL)
   parser.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
   parser.add_argument('-D', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
 
@@ -55,61 +59,103 @@ def main(argv):
   run(args.input, **vars(args))
 
 
-def run(infile, human=False, all_repeats=False, *nargs, **kwargs):
+def run(infile, qual_thres=0, qual_format='sanger', human=False, all_repeats=False, *nargs, **kwargs):
   for family in parse(infile):
     for order in ('ab', 'ba'):
       for mate in (0, 1):
-        alignment = family[order][mate]
-        if alignment:
-          consensus, errors, repeat_errors, new_align = compare(alignment, all_repeats=all_repeats,
-                                                                make_new_align=human)
-          if human:
-            for seq, seq_errors in zip(new_align, errors):
-              print('{} errors: {}'.format(seq, seq_errors))
-            if all_repeats:
-              print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors),
-                                                                ', '.join(map(str, repeat_errors))))
-            else:
-              print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors), repeat_errors))
-          elif all_repeats:
-            print(family['bar'], order, mate, len(alignment), *repeat_errors, sep='\t')
+        seq_align, qual_align = family[order][mate]
+        if not seq_align:
+          continue
+        consensus, errors, repeat_errors, new_align = compare(seq_align,
+                                                              qual_align,
+                                                              thres=qual_thres,
+                                                              qual_format=qual_format,
+                                                              all_repeats=all_repeats,
+                                                              make_new_align=human)
+        if human:
+          for seq, seq_errors in zip(new_align, errors):
+            print('{} errors: {}'.format(seq, seq_errors))
+          if all_repeats:
+            print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors),
+                                                              ', '.join(map(str, repeat_errors))))
           else:
-            print(family['bar'], order, mate, len(alignment), repeat_errors, *errors, sep='\t')
+            print('{} errors: {}, repeat errors: {}\n'.format(consensus, sum(errors), repeat_errors))
+        elif all_repeats:
+          print(family['bar'], order, mate, len(seq_align), *repeat_errors, sep='\t')
+        else:
+          print(family['bar'], order, mate, len(seq_align), repeat_errors, *errors, sep='\t')
 
 
 def parse(infile):
+  """Parse a families.msa.tsv file.
+  Yields a data structure for each family:
+  family = {
+    'bar': barcode,                                     # family 'AAACCGACACAGGACTAGGGATCA'
+    'ab': (                                               # order ab
+            ([seq1, seq2, seq3], [quals1, quals2, quals3]), # mate 1
+            ([seq1, seq2], [quals1, quals2]),               # mate 2
+          ),
+    'ba': (                                               # order ba
+            ([seq1, seq2], [quals1, quals2]),               # mate 1
+            ([], []),                                       # mate 2
+          )
+  }
+  That is, each family is a dict with the 'bar' key giving the barcode sequence, and a key for both
+  orders ('ab' and 'ba'). The value for each order is a tuple of 2 values, one for each mate. Each
+  value in the tuple is itself a 2-tuple containing the aligned bases and quality scores.
+  Examples:
+  Getting the sequences for mate 1 of order "ab":
+  seq_align = family['ab'][0][0]
+  Getting the quality scores:
+  seq_align = family['ab'][0][1]
+  Getting the sequences for mate 2 of order "ba":
+  seq_align = family['ba'][1][0]
+  """
   last_barcode = None
-  family = {'bar':None, 'ab':([], []), 'ba':([], [])}
+  family = {'bar':None, 'ab':(([],[]), ([],[])), 'ba':(([],[]), ([],[]))}
   for line in infile:
-    fields = line.split('\t')
+    fields = line.rstrip('\r\n').split('\t')
     barcode = fields[0]
     order = fields[1]
     mate = int(fields[2])-1
     seq = fields[4]
+    quals = fields[5]
     if barcode != last_barcode:
       if last_barcode is not None:
         yield family
-      family = {'bar':barcode, 'ab':([], []), 'ba':([], [])}
+      family = {'bar':None, 'ab':(([],[]), ([],[])), 'ba':(([],[]), ([],[]))}
       last_barcode = barcode
-    family[order][mate].append(seq)
+    family[order][mate][0].append(seq)
+    family[order][mate][1].append(quals)
   yield family
 
 
-def compare(alignment, all_repeats=False, make_new_align=False):
-  num_seqs = len(alignment)
+def compare(seq_align, qual_align=None, thres=None, qual_format='sanger', all_repeats=False,
+            make_new_align=False):
   consensus = ''
+  thres_char = phred_to_qual_char(thres, qual_format)
+  num_seqs = len(seq_align)
+  if qual_align is None:
+    qual_align = [''] * num_seqs
   new_align = [''] * num_seqs
   errors = [0] * num_seqs
+  last_qual = chr(126)
   if all_repeats:
     repeat_errors = []
   else:
     repeat_errors = 0
-  for bases in zip(*alignment):
+  for bases, quals in zip(zip(*seq_align), zip(*qual_align)):
     # Tally how many of each base there are at this position, and find the "winner" (the consensus).
     votes = {'A':0, 'C':0, 'G':0, 'T':0, '-':0, 'N':0}
     cons = 'N'
     max_vote = 0
-    for base in bases:
+    for base, qual in zip(bases, quals):
+      #TODO: Get gap quality score using algorithm in consensus.c.
+      if qual == ' ':
+        qual = last_qual
+      last_qual = qual
+      if qual < thres_char:
+        continue
       vote = votes[base]
       vote += 1
       if vote == max_vote:
@@ -119,8 +165,14 @@ def compare(alignment, all_repeats=False, make_new_align=False):
         cons = base
       votes[base] = vote
     # Make new alignment without consensus bases.
-    for i, base in enumerate(bases):
-      if base == cons:
+    last_qual = chr(126)
+    for i, (base, qual) in enumerate(zip(bases, quals)):
+      if qual == ' ':
+        qual = last_qual
+      last_qual = qual
+      if qual < thres_char:
+        new_align[i] += ' '
+      elif base == cons:
         new_align[i] += '.'
       else:
         new_align[i] += base
@@ -135,6 +187,16 @@ def compare(alignment, all_repeats=False, make_new_align=False):
     consensus += cons
     i += 1
   return consensus, errors, repeat_errors, new_align
+
+
+def phred_to_qual_char(phred, qual_format):
+  # Based on the trusty "standard": https://en.wikipedia.org/wiki/Fastq#Encoding
+  if qual_format == 'solexa' or qual_format == 'illumina1.3' or qual_format == 'illumina1.5':
+    return chr(phred+64)
+  if qual_format == 'sanger' or qual_format == 'illumina1.8':
+    return chr(phred+33)
+  else:
+    raise ValueError('Unrecognized qual_format "{}"'.format(qual_format))
 
 
 def tone_down_logger():
